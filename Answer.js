905affcb6c2b60b0c2f33ed4eba25545a4a7bac5
Julian Veling <julian_veling@hotmail.com>
2015-06-17 19:19:51 +0100

    refactored

--- Answer.js
+++ Answer.js
@@ -4,23 +4,11 @@ var $ = function( selector ) {
   // Your code here //
   ////////////////////
 
-  var regEx = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/;
+
   var elements = [];
-  var match, elem;
-
-  var finders = {
-    ID: /#((?:[\w\u00c0-\uFFFF-]|\\.)+)/,
-    CLASS: /\.((?:[\w\u00c0-\uFFFF-]|\\.)+)/,
-    TAG: /^((?:[\w\u00c0-\uFFFF\*-]|\\.)+)/
-  };  
-
-  function deeperMatch(){
-    match = []
-    match.push(finders.ID.exec(selector));
-    match.push(finders.CLASS.exec(selector));
-    match.push(finders.TAG.exec(selector));
-    return match;
-  }
+  var regEx = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/;
+  var tagsRegEx = /^((?:[\w\u00c0-\uFFFF\*-]|\\.)+)/;
+  var match, elem, tags;
 
   if ( match = regEx.exec( selector ))  {
     if ( (elem = match[1]) ) {
@@ -31,8 +19,8 @@ var $ = function( selector ) {
       elements = document.getElementsByClassName( elem );
     }
   } else {
-    match = deeperMatch();
-    var tags = document.getElementsByTagName(match[2][1]);
+    match = tagsRegEx.exec(selector);
+    tags = document.getElementsByTagName(match[1]);
     for(i = 0; i < tags.length; i++){
       if(tags[i].id){  
         elements.push(tags[i]);
